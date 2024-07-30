d18f4ac28decefb7444430c93b109375383502bb
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-30 16:55:40 -0400

    Method-override bug fixed

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-import dotenv from 'dotenv'
-dotenv.config();
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express"
+import dotenv from "dotenv" // require package
+import mongoose from "mongoose" 
 import Fruit from "./models/fruit.js"
+import methodOverride from "method-override"
+import logger from "morgan"
 
+// Mount it along with our other middleware, ABOVE the routes
+dotenv.config(); // Loads the environment variables from .env file
 const app = express();
-
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); 
+app.use(logger("dev")); 
+
 
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -18,10 +23,37 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render("fruits/index.ejs", { fruits: allFruits });
+  });
+  
+
 
 app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
   });
+
+  // GET /fruits
+app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.send("Welcome to the index page!");
+  });
+
+  app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+  });
+
+  // GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    console.log(foundFruit);
+    res.send(`This is the edit route for ${foundFruit.name}`);
+  });
+  
+  
+  
 
   // server.js
 
@@ -42,8 +74,34 @@ app.post("/fruits", async (req, res) => {
       req.body.isReadyToEat = false;
     }
     await Fruit.create(req.body);
-    res.redirect("/fruits/new");
+    res.redirect("/fruits");
   });
+
+  app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.send(
+      `This route renders the show page for fruit id: ${req.params.fruitId}!`
+    );
+  });
+
+  // server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+  });
+  
+  
   
   
   
