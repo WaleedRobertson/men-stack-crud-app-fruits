318ce325c173f90cb45e8bbd49b0b5452abcd3e6
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-30 14:03:42 -0400

    POST HTML Method added

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
 import mongoose from 'mongoose'
+import Fruit from "./models/fruit.js"
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -13,6 +17,36 @@ mongoose.connect(process.env.MONGODB_URI);
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
+
+
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+  });
+
+  // server.js
+
+/* POST /fruits
+// app.post("/fruits", async (req, res) => {
+//     console.log(req.body);
+//     //talk to the DB - through the model 
+//     res.redirect("/fruits/new");
+//   })*/
+
+  // server.js
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+  });
+  
+  
+  
 
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
