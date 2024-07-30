1b0fc548c043dd8a92a6b2b41cf718f2989104a2
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-30 13:06:24 -0400

    MongoDB connected

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-import dotenv from dotenv
+import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
-import mongoose from mongoose 
+import mongoose from 'mongoose'
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 // server.js
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
-  
+});
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+    console.log("Listening on port 3000");
 });
 
